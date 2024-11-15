2de0f49af9080e2e95823038d9603566cd371f88
ManInMyVan <maninmyvan@gmail.com>
2024-11-15 15:04:04 -0700

    add permission for /grim stopspectating here

--- src/main/java/ac/grim/grimac/commands/GrimStopSpectating.java
+++ src/main/java/ac/grim/grimac/commands/GrimStopSpectating.java
@@ -3,6 +3,8 @@ package ac.grim.grimac.commands;
 import ac.grim.grimac.GrimAPI;
 import ac.grim.grimac.utils.anticheat.MessageUtil;
 import co.aikar.commands.BaseCommand;
+import co.aikar.commands.BukkitCommandCompletionContext;
+import co.aikar.commands.CommandCompletions.CommandCompletionHandler;
 import co.aikar.commands.annotation.CommandAlias;
 import co.aikar.commands.annotation.CommandCompletion;
 import co.aikar.commands.annotation.CommandPermission;
@@ -10,16 +12,21 @@ import co.aikar.commands.annotation.Subcommand;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import java.util.List;
+
 @CommandAlias("grim|grimac")
 public class GrimStopSpectating extends BaseCommand {
+    public static final CommandCompletionHandler<BukkitCommandCompletionContext> completionHandler = context -> context.getSender().hasPermission("grim.spectate.stophere") ? List.of("here") : List.of();
+
     @Subcommand("stopspectating")
     @CommandPermission("grim.spectate")
-    @CommandCompletion("here")
+    @CommandCompletion("@stopspectating")
     public void onStopSpectate(CommandSender sender, String[] args) {
         String string = args.length > 0 ? args[0] : null;
-        if (!(sender instanceof Player player)) return;
+        if (!(sender instanceof Player)) return;
+        Player player = (Player) sender;
         if (GrimAPI.INSTANCE.getSpectateManager().isSpectating(player.getUniqueId())) {
-            boolean teleportBack = string == null || !string.equalsIgnoreCase("here");
+            boolean teleportBack = string == null || !string.equalsIgnoreCase("here") || !sender.hasPermission("grim.spectate.stophere");
             GrimAPI.INSTANCE.getSpectateManager().disable(player, teleportBack);
         } else {
             String message = GrimAPI.INSTANCE.getConfigManager().getConfig().getStringElse("cannot-spectate-return", "%prefix% &cYou can only do this after spectating a player.");
