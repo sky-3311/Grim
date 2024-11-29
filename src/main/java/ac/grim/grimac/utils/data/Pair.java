7fe224c2063c58d0bb1138e726c061887fb7bed6
ManInMyVan <113963219+ManInMyVan@users.noreply.github.com>
2024-11-28 17:18:58 -0700

    cleanup Pair class

--- src/main/java/ac/grim/grimac/utils/data/Pair.java
+++ src/main/java/ac/grim/grimac/utils/data/Pair.java
@@ -18,20 +18,4 @@
 
 package ac.grim.grimac.utils.data;
 
-import java.util.Objects;
-
-public record Pair<A, B>(A first, B second) {
-
-    public static <T, K> Pair<T, K> of(T a, K b) {
-        return new Pair<>(a, b);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (!(o instanceof Pair)) {
-            return false;
-        }
-        Pair b = (Pair) o;
-        return Objects.equals(this.first, b.first) && Objects.equals(this.second, b.second);
-    }
-}
+public record Pair<A, B>(A first, B second) {}
